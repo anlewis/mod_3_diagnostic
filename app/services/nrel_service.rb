067b0612368aefce0bedcc2a2e76ea217d9f5bc6
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-07 10:12:15 -0700

    add 6 mile radius limitation, still need to test

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -5,7 +5,7 @@ class NrelService
       faraday.adapter Faraday.default_adapter
     end
 
-    response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_key']}&location=#{zip_code}&fuel_type=ELEC,LPG&limit=10")
+    response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_key']}&location=#{zip_code}&fuel_type=ELEC,LPG&limit=10&radius=6.0")
 
     JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
   end
